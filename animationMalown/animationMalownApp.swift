fd9b8e8d9c290557fb949a81d3705c87aee18ec1
Marine M <marine.michelot-cic@ibm.com>
2023-06-05 22:11:00 +0200

    Initial Commit

---
+++ animationMalown/animationMalownApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationMalownApp.swift
+//  animationMalown
+//
+//  Created by Marine Michelot on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct animationMalownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
